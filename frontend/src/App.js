a5cc727c5f34fc7b4e59e16d9a719edd88196b58
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2023-01-30 13:17:54 +0000

    Basic login frontend created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import {Routes,Route} from "react-router-dom";
 import MaterialDashboard from './components/material_dashboard/material_dashboard';
 import SignUp from './components/signup/signup'
+import Login from'./components/login/login'
 
 function App() {
+
   return (
+    
     <Routes>
       <Route path='/' element={<SignUp/>}/>
       <Route path='/materials' element={<MaterialDashboard/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>
   );
 }
---
+++ frontend/src/components/login/login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <h3>Email</h3>
+                <input type="text"></input>
+
+                <h3>Password</h3>
+                <input type="text"></input>
+            </form>
+        </div>
+    )
+}
+
+export default Login
